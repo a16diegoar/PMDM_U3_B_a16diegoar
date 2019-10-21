16e1410d5ec195e199c2f65ca02fe9fe32ae71b1
Diego Antelo Rutolo <a16diegoar@iessaclemente.net>
2019-10-21 10:16:04 +0200

    Toast respuesta en Entrada txt

--- app/src/main/java/com/example/u3_b_a16diegoar/fragments/TxtinDfrag.java
+++ app/src/main/java/com/example/u3_b_a16diegoar/fragments/TxtinDfrag.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,6 +26,16 @@ public class TxtinDfrag extends DialogFragment {
         btnAc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText txeNom = rootView.findViewById(R.id.txeNom);
+                EditText txePasswd = rootView.findViewById(R.id.txePasswd);
+
+                String txt = "Usuario: \"";
+                txt += txeNom.getText();
+                txt += "\"; Contrasinal: \"";
+                txt += txePasswd.getText();
+                txt += "\"";
+
+                Toast.makeText(getContext(), txt, Toast.LENGTH_LONG).show();
                 dismiss();
             }
         });
